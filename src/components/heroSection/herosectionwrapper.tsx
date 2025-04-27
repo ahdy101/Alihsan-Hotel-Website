a63d452dedceb1de01fba010073afd03e7f7d8e8
ahdy101 <ahmadomniverse@gmail.com>
2025-04-27 09:53:26 -1200

    refactor(hero): Create wrapper for server-side hero section

---
+++ src/components/heroSection/herosectionwrapper.tsx
@@ -0,0 +1,8 @@
+import HeroSection from './herosection';
+
+const HeroSectionWrapper = () => {
+  // This can fetch data or handle other server-side logic
+  return <HeroSection />;
+};
+
+export default HeroSectionWrapper;
